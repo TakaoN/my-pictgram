fad360958ea8a530342e5419d90953e637c405c6
Takao Nakada <acnakada@gmail.com>
2018-10-22 07:05:39 +0900

    first commit

---
+++ app/models/topic.rb
@@ -0,0 +1,14 @@
+class Topic < ApplicationRecord
+  validates :user_id, presence: true
+  validates :description, presence: true
+  validates :image, presence: true
+
+  mount_uploader :image, ImageUploader
+
+  belongs_to :user
+
+  has_many :favorites
+  has_many :favorite_users, through: :favorites, source: 'user'
+  has_many :comments, ->{ order( created_at: :DESC) }
+  has_many :comment_users, through: :comments, source: 'user'
+end
---
+++ config/routes.rb
@@ -0,0 +1,20 @@
+Rails.application.routes.draw do
+  get 'sessions/new'
+
+  root 'pages#index'
+  get 'pages/help'
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
+  resources :users
+  resources :topics
+
+  get 'favorites/index'
+  post '/favorites', to: 'favorites#create'
+
+  get 'comments/index'
+  get 'comments/new'
+  post '/comments', to: 'comments#create'
+end
