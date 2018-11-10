8298020349c5b2da37ad8cd37dd5736a5907006e
Takao Nakada <acnakada@gmail.com>
2018-11-10 16:25:07 +0900

    1st

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -14,7 +14,6 @@ class CommentsController < ApplicationController
 
   def create
     @comment = current_user.comments.new(comment_params)
-
     if @comment.save
       redirect_to topics_path, success: 'コメントを登録しました'
     else
