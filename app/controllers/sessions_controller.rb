615081904cd9a1505bc12d52df3b9fb8f148d7b1
Takao Nakada <acnakada@gmail.com>
2018-12-14 20:32:45 +0900

    jquery

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email])
+    #binding.pry
+    #user = User.find_by(email: params[:session][:email])
+    user = User.find_by(email_params) # これは通る
     if user && user.authenticate(params[:session][:password])
+    #user = User.find_by(email: email_params) # これはエラーになる
+    #if user && user.authenticate(password_params) # これはエラーになる
       log_in user
       redirect_to root_path, success: 'ログインに成功しました'
     else
@@ -19,12 +23,21 @@ class SessionsController < ApplicationController
   end
 
   private
-  def log_in(user)
-    session[:user_id] = user.id
-  end
+    # これは意味ある？
+    def email_params
+      params.require(:session).permit(:email)
+    end
+    # これは意味ある？
+    #def password_params
+    #  params.require(:session).permit(:password)
+    #end
 
-  def log_out
-    session.delete(:user_id)
-    @current_user = nil
-  end
+    def log_in(user)
+      session[:user_id] = user.id
+    end
+
+    def log_out
+      session.delete(:user_id)
+      @current_user = nil
+    end
 end
